61cab491290fc2b6ec4bbb40cdc9d233dc7b698c
pedro almeida <pedro_msca2002@hotmail.com>
2023-10-02 19:21:58 +0100

    Alínea 4 concluída

--- project/src/test/java/fileIO/CarregadoresPaisTest.java
+++ project/src/test/java/fileIO/CarregadoresPaisTest.java
@@ -1,2 +1,13 @@
-package fileIO;public class CarregadoresPais {
+package fileIO;
+
+import org.junit.jupiter.api.Test;
+
+public class CarregadoresPaisTest implements Files{
+
+    @Test
+    void getChargersCountry() {
+        CarregadoresCidade carregadoresCidade = new CarregadoresCidade();
+        carregadoresCidade.GetChargers(CarregadoresFile);
+        carregadoresCidade.contaCarregadorPais();
+    }
 }
