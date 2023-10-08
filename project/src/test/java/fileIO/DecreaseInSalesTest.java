0c1756eca7132a406cd493a061cc9dc73b0b04ca
José Pedro Pina <>
2023-10-08 21:49:04 +0100

    no message

---
+++ project/src/test/java/fileIO/DecreaseInSalesTest.java
@@ -0,0 +1,16 @@
+package fileIO;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class DecreaseInSalesTest {
+
+    //! Falta assert
+    @Test
+    void test1() throws IOException {
+        /**DecreaseInSales decreaseInSales = new DecreaseInSales();
+        decreaseInSales.getData();
+        decreaseInSales.print();*/
+    }
+}
